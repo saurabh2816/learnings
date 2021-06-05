37c95b7033209fccf7f56183b6f0356eea4b7543
saurabh2816 <saurabh2816#>
2021-06-05 14:09:25 -0700

    Lambdas vs Interface Implementations

---
+++ Java8/src/Google.java
@@ -0,0 +1,19 @@
+public class Google {
+
+    public static void main(String[] args) {
+
+        // myLambda is providing an implementation of the interface function: foo()
+        PaymentService myLambda = () -> System.out.println("This is a lambda fx");
+        myLambda.pay();
+
+        // how we usually do it
+        PaymentService payObject = new PaymentServiceImpl();
+        payObject.pay();
+
+
+    }
+}
+
+interface lambdaInterface {
+    void foo();
+}
---
+++ Java8/src/PaymentServiceImpl.java
@@ -0,0 +1,6 @@
+public class PaymentServiceImpl implements PaymentService {
+    @Override
+    public void pay() {
+        System.out.println("Payment done!!");
+    }
+}
